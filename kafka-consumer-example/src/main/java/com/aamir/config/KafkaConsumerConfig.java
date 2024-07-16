05ed95a3c99cde4a427f8f8d96c648ab42cf55c4
mohammadaamir1102 <er.aamir1102@gmail.com>
2024-07-16 13:55:10 +0530

    Java level config of Serialization and DeSerialization

--- kafka-consumer-example/src/main/java/com/aamir/config/KafkaConsumerConfig.java
+++ kafka-consumer-example/src/main/java/com/aamir/config/KafkaConsumerConfig.java
@@ -2,7 +2,6 @@ package com.aamir.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -11,7 +10,6 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
