5ac054d26c10f5deb78eb2271feb6df68dfa9ae1
mohammadaamir1102 <er.aamir1102@gmail.com>
2024-07-16 13:47:41 +0530

    Java level config of Serialization and DeSerialization

--- kafka-producer-example/src/main/java/com/aamir/config/KafkaProducerConfig.java
+++ kafka-producer-example/src/main/java/com/aamir/config/KafkaProducerConfig.java
@@ -1,8 +1,17 @@
 package com.aamir.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -17,5 +26,24 @@ public class KafkaProducerConfig {
     public NewTopic pojoTopic(){
         return new
                 NewTopic("pojo-topic",3, (short) 1);
+    }
+
+    @Bean
+    public Map<String,Object> producerConfiguration(){
+        Map<String,Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String,Object> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfiguration());
+    }
+
+    @Bean
+    public KafkaTemplate<String,Object> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
     }
 }
